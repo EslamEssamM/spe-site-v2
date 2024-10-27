f89463c1cf053d741bd023c60cf6787f39c966f2
EslamEssamM <ee3@pme.suezuni.edu.eg>
2024-10-27 07:54:33 +0300

    Refactor file route for magazine pages

--- src/routes/magazines.$id.tsx
+++ src/routes/magazines.$id.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import MagazinePage from "@/pages/magazinePage";
+import MagazinePage from "@/pages/MagazinePage";
 // @ts-ignore
 export const Route = createFileRoute("/magazines/$id")({
   component: MagazinePage,
