d3e4a040e11d7ae67d6e4e44265d139eddbeecdb
EslamEssamM <ee3@pme.suezuni.edu.eg>
2024-10-27 07:53:06 +0300

    Refactor Tooltip component import to fix case sensitivity issue

--- src/routes/magazines.$id.tsx
+++ src/routes/magazines.$id.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import MagazinePage from "@/pages/magazinePage";
-
+// @ts-ignore
 export const Route = createFileRoute("/magazines/$id")({
   component: MagazinePage,
 });
